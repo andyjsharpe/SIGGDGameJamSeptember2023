355894d1a856226574264be1b042f9d314dea1aa
andyjsharpe <ajsusa1@gmail.com>
2023-09-19 11:53:45 -0400

    Auto Culler Changes

--- Assets/Scripts/AutoCuller.cs
+++ Assets/Scripts/AutoCuller.cs
@@ -1,28 +1,33 @@
-using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class AutoCuller : MonoBehaviour
 {
+    private Transform _playerTrans;
+    
     private ShadowCaster2D _caster;
     private SpriteRenderer _renderer;
     private Animator _animator;
     private PolygonCollider2D _polygonCollider2D;
     private BoxCollider2D _boxCollider2D;
     private Health _health;
+    private PersonControl _personControl;
+    private Rigidbody2D _rigidbody2D;
 
     //TODO: Make faster
     // Start is called before the first frame update
     private void Start()
     {
-        /*
+        _playerTrans = FindObjectOfType<AlienControl>().transform;
+        
         _caster = GetComponent<ShadowCaster2D>();
         _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
         _polygonCollider2D = GetComponent<PolygonCollider2D>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
         _health = GetComponent<Health>();
-        */
+        _personControl = GetComponent<PersonControl>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void OnBecameInvisible()
@@ -61,66 +66,20 @@ public class AutoCuller : MonoBehaviour
         {
             _boxCollider2D.enabled = b;
         }
-    }
+        if (_personControl != null)
+        {
+            _personControl.enabled = b;
+        }
 
-    /*
+        if (_rigidbody2D != null)
+        {
+            _rigidbody2D.simulated = b;
+        }
+    }
+    
     // Update is called once per frame
     private void Update()
     {
-        if ((_playerTrans.position - transform.position).sqrMagnitude > 144)
-        {
-            if (_caster is not null)
-            {
-                _caster.enabled = false;
-            }
-            if (_renderer is not null)
-            {
-                _renderer.enabled = false;
-            }
-            if (_animator is not null)
-            {
-                _animator.enabled = false;
-            }
-            if (_polygonCollider2D is not null)
-            {
-                _polygonCollider2D.enabled = false;
-            }
-            if (_health is not null)
-            {
-                _health.enabled = false;
-            }
-            if (_boxCollider2D is not null)
-            {
-                _boxCollider2D.enabled = false;
-            }
-        }
-        else
-        {
-            if (_caster is not null)
-            {
-                _caster.enabled = true;
-            }
-            if (_renderer is not null)
-            {
-                _renderer.enabled = true;
-            }
-            if (_animator is not null)
-            {
-                _animator.enabled = true;
-            }
-            if (_polygonCollider2D is not null)
-            {
-                _polygonCollider2D.enabled = true;
-            }
-            if (_health is not null)
-            {
-                _health.enabled = true;
-            }
-            if (_boxCollider2D is not null)
-            {
-                _boxCollider2D.enabled = true;
-            }
-        }
+        OnOff((_playerTrans.position - transform.position).sqrMagnitude > 144);
     }
-    */
 }
